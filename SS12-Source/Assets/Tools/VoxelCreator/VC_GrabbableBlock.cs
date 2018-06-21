0db049ded201495704afa106cfc4ca7214c69296
Thomas Bartlett <tbartl@umich.edu>
2018-06-21 18:43:27 -0400

    Starting work on Voxel Creator

---
+++ SS12-Source/Assets/Tools/VoxelCreator/VC_GrabbableBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VC_GrabbableBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
