13bbe06325dea6b9c0207767a247f62da3ab94d1
Thomas Bartlett <tbartl@umich.edu>
2018-06-26 21:25:33 -0400

    Basic grabbing/releasing

---
+++ SS12-Source/Assets/Objects/Grabbable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Grabbable : MonoBehaviour {
+	
+	Rigidbody rb;
+	Hand grabber;
+
+	void Awake() {
+		rb = this.GetComponent<Rigidbody>();
+	}
+
+	public bool IsGrabbable() {
+		return (grabber == false);
+	}
+
+	public void Grab(Hand newGrabber) {
+		grabber = newGrabber;
+		rb.isKinematic = true;
+		this.transform.parent = grabber.transform;
+	}
+
+	public void Release() {
+		grabber = null;
+		rb.isKinematic = false;
+		this.transform.parent = null;
+	}
+}
---
+++ SS12-Source/Assets/Player/Hand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    Grabbable hoverObject;
+    Grabbable holdObject;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && hoverObject && hoverObject.IsGrabbable() && !holdObject) {
+            hoverObject.Grab(this);
+            holdObject = hoverObject;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && holdObject) {
+            holdObject.Release();
+            holdObject = null;
+        }
+    }
+
+    void FixedUpdate() {
+        hoverObject = null;
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (holdObject)
+            return;
+
+        Grabbable otherGrabbable = other.GetComponent<Grabbable>();
+        if (!otherGrabbable)
+            return;
+
+        if (!otherGrabbable.IsGrabbable())
+            return;
+
+        hoverObject = otherGrabbable;
+    }
+}
