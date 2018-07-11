7d91174d103cd2878ddb90dbec812afc3d195af0
Thomas Bartlett <tbartl@umich.edu>
2018-07-10 23:20:50 -0400

    Screwdriver, debug toggle gravity,

---
+++ SS5R-Source/Assets/DebugToggleGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugToggleGravity : MonoBehaviour {
+
+    Rigidbody rb;
+
+    void Awake() {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.useGravity = !rb.useGravity;
+        }
+    }
+}
