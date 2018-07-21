e7ad237f2d45562202f65d0371572c73fd05e0f5
Thomas Bartlett <tbartl@umich.edu>
2018-07-21 09:10:29 -0400

    Position body

---
+++ SS5R-Source/Assets/Objects/Body/BodyPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPosition : MonoBehaviour {
+
+    public Transform head;
+
+    public float zOffSet;
+
+    public AnimationCurve pitchCurve;
+
+    // Update is called once per frame
+    void Update() {
+        if (!head)
+            return;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(0, head.transform.rotation.eulerAngles.y, 0), Time.deltaTime * 2);
+        float xRot = head.transform.rotation.eulerAngles.x;
+        Vector3 pitchOffset = Vector3.zero;
+        if (xRot > 0 && xRot <= 120) {
+            float pRot = xRot / 90;
+            Debug.Log(pRot);
+            pitchOffset -= this.transform.forward * pitchCurve.Evaluate(pRot);
+            pitchOffset += Vector3.up * pitchCurve.Evaluate(pRot);
+        }
+        this.transform.position = head.transform.position + Vector3.down * zOffSet + pitchOffset;
+    }
+}
