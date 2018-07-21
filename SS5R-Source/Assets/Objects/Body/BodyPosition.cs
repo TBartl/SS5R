a1aa50133a9c665ed5c62efcf948b713ec7bfde9
Thomas Bartlett <tbartl@umich.edu>
2018-07-21 09:54:11 -0400

    Body position pitch/roll adjustments

--- SS5R-Source/Assets/Objects/Body/BodyPosition.cs
+++ SS5R-Source/Assets/Objects/Body/BodyPosition.cs
@@ -6,23 +6,34 @@ public class BodyPosition : MonoBehaviour {
 
     public Transform head;
 
-    public float zOffSet;
+    public Vector3 baseOffset = new Vector3(0, -.46f, -.147f);
 
-    public AnimationCurve pitchCurve;
+    public AnimationCurve pitchOffsetCurve;
+    public AnimationCurve rollOffsetCurve;
 
     // Update is called once per frame
     void Update() {
         if (!head)
             return;
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(0, head.transform.rotation.eulerAngles.y, 0), Time.deltaTime * 2);
-        float xRot = head.transform.rotation.eulerAngles.x;
+
+
         Vector3 pitchOffset = Vector3.zero;
-        if (xRot > 0 && xRot <= 120) {
-            float pRot = xRot / 90;
-            Debug.Log(pRot);
-            pitchOffset -= this.transform.forward * pitchCurve.Evaluate(pRot);
-            pitchOffset += Vector3.up * pitchCurve.Evaluate(pRot);
-        }
-        this.transform.position = head.transform.position + Vector3.down * zOffSet + pitchOffset;
+        float xRot = head.transform.rotation.eulerAngles.x;
+        if (xRot > 180)
+            xRot -= 360;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+        float pXRot = xRot / 90;
+        pitchOffset -= this.transform.forward * pitchOffsetCurve.Evaluate(pXRot);
+        pitchOffset += Vector3.up * pitchOffsetCurve.Evaluate(pXRot);
+
+        float zRot = head.transform.rotation.eulerAngles.z;
+        if (zRot > 180)
+            zRot -= 360;
+        float pZRot = zRot / 90;
+        pitchOffset += Mathf.Sign(pZRot) * this.transform.right * rollOffsetCurve.Evaluate(Mathf.Abs(pZRot));
+        pitchOffset += this.transform.up * rollOffsetCurve.Evaluate(Mathf.Abs(pZRot));
+
+        this.transform.position = head.transform.position + this.transform.TransformVector(baseOffset) + pitchOffset;
     }
 }
