77057c6b512fec90c43551c62e7744b564fcd193
Thomas Bartlett <tbartl@umich.edu>
2018-08-17 19:55:04 -0400

    Intercom improvements and flicker

---
+++ SS5R-Source/Assets/Environment/Other/IntercomFlicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntercomFlicker : MonoBehaviour {
+
+	float delay = .1f;
+
+	IEnumerator Start() {
+		while (true) {
+			yield return new WaitForSeconds(delay);
+			this.transform.localRotation = Quaternion.Euler(0, 0, 180);
+			yield return new WaitForSeconds(delay);
+			this.transform.localScale = new Vector3(1, -1, 1);
+			yield return new WaitForSeconds(delay);
+			this.transform.localRotation = Quaternion.Euler(0, 0, 0);
+			yield return new WaitForSeconds(delay);
+			this.transform.localScale = Vector3.one;
+		}
+	}
+
+}
