585b77f2b290617f8f21a28c48897b40dd515e29
Thomas Bartlett <tbartl@umich.edu>
2018-08-18 00:09:48 -0400

    Pad movement, toolbox material fix

---
+++ SS5R-Source/Assets/Player/PadMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadMovement : MonoBehaviour {
+	float speed = 3;
+	Camera mainCam;
+
+    Controller controller;
+    void Awake() {
+        controller = this.GetComponent<Controller>();
+		mainCam = FindObjectOfType<Camera>();
+    }
+
+    void Update() {
+		Vector2 dir = controller.Get.GetAxis();
+		Vector3 dir3 = new Vector3(dir.x, 0, dir.y);
+		dir3 = Quaternion.AngleAxis(mainCam.transform.rotation.eulerAngles.y, Vector3.up) * dir3;
+		transform.parent.position += dir3 * Time.deltaTime * speed;
+    }
+}
