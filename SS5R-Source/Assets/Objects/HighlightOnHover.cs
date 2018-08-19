4170e9183f1340f26682e4efe313b367c50e596f
Thomas Bartlett <tbartl@umich.edu>
2018-08-19 12:16:47 -0400

    Allow highlight to work on child meshes

--- SS5R-Source/Assets/Objects/HighlightOnHover.cs
+++ SS5R-Source/Assets/Objects/HighlightOnHover.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Interactable))]
 public class HighlightOnHover : MonoBehaviour, IOnHovered {
 
     public Material highlightMaterial;
