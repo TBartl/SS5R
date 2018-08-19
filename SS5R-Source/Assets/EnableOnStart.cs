6537f3a2ab7d57eaa65ec70f23dd5baa05424f37
Thomas Bartlett <tbartl@umich.edu>
2018-08-18 20:27:42 -0400

    Updated lights, only do screen effects while playing

---
+++ SS5R-Source/Assets/EnableOnStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnStart : MonoBehaviour {
+
+	public Behaviour component;
+
+	// Use this for initialization
+	void Start () {
+		component.enabled = true;
+	}
+}
