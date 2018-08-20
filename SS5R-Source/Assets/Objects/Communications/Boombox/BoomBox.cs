a6cdf2a46eee415b5a6b39c3669fd71a19c71f7f
Thomas Bartlett <tbartl@umich.edu>
2018-08-19 22:30:42 -0400

    More functional boombox

--- SS5R-Source/Assets/Objects/Communications/Boombox/BoomBox.cs
+++ SS5R-Source/Assets/Objects/Communications/Boombox/BoomBox.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoomBox : MonoBehaviour {
-    int index = 0;
+    public int index = 0;
     public AudioClip[] clips;
 
     AudioSource source;
+	Text text;
 
     public AudioClip overrideBwoink;
 
 	void Awake() {
 		source = this.GetComponent<AudioSource>();
+		text = this.GetComponentInChildren<Text>();
+		Play();
 	}
 
     public void Forward() {
@@ -28,9 +32,11 @@ public class BoomBox : MonoBehaviour {
 
     public void Play() {
         source.Play();
+		text.text = source.clip.name + ".ogg";
     }
 
     public void Stop() {
+		text.text = "";
         if (!overrideBwoink) {
             source.Stop();
         } else {
