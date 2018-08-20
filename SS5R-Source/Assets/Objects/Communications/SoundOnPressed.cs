731716d25b7dedcdce7d5050eb97a4d815b31ee3
Thomas Bartlett <tbartl@umich.edu>
2018-08-19 22:17:59 -0400

    Click on button press

---
+++ SS5R-Source/Assets/Objects/Communications/SoundOnPressed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnPressed : MonoBehaviour, IOnPressed {
+    public AudioClip clip;
+
+    public void OnPressed(Interactor by) {
+        AudioSource source = this.gameObject.AddComponent<AudioSource>();
+        Destroy(source, clip.length + 1);
+        source.clip = clip;
+        source.spatialBlend = 1;
+        source.Play();
+    }
+
+}
