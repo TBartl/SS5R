d8c9ca08aa8ec49c32b9c8b051cf7e17d24b9786
Thomas Bartlett <tbartl@umich.edu>
2018-08-22 23:12:50 -0400

    Air scrubber

---
+++ SS5R-Source/Assets/Environment/Other/RotateOverTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour {
+
+    public Vector3 amount;
+
+    void Update() {
+        this.transform.rotation *= Quaternion.Euler(amount * Time.deltaTime);
+    }
+}
