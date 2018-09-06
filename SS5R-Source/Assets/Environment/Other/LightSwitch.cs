e5e8e53622d1df3127b9d6e24c582b638acc776f
Thomas Bartlett <tbartl@umich.edu>
2018-09-05 22:38:34 -0400

    LightSwitch

---
+++ SS5R-Source/Assets/Environment/Other/LightSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour, IOnPressed {
+    public bool on = true;
+    float transitionTime = .14f;
+    float transitionPercent = 1;
+
+    Coroutine transition;
+
+    public List<Light> lights;
+
+    void Awake() {
+        UpdateLights();
+    }
+
+    public void OnPressed(Interactor by) {
+        if (transition != null) StopCoroutine(transition);
+        on = !on;
+        UpdateLights();
+        transition = StartCoroutine(Flip());
+    }
+
+    IEnumerator Flip() {
+        Quaternion fromRot = this.transform.localRotation;
+        Quaternion toRot = Quaternion.identity;
+        if (!on)
+            toRot = Quaternion.Euler(-32, 0, 0);
+        for (float t = (1 - transitionPercent) * transitionTime; t < transitionTime; t += Time.deltaTime) {
+            transitionPercent = t / transitionTime;
+            this.transform.localRotation = Quaternion.Slerp(fromRot, toRot, transitionPercent);
+            yield return null;
+        }
+        this.transform.localRotation = toRot;
+        transitionPercent = 1;
+    }
+
+    void UpdateLights() {
+        foreach (Light light in lights) {
+            light.enabled = on;
+        }
+    }
+}
---
+++ SS5R-Source/Assets/Misc/EnableOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnStart : MonoBehaviour {
+
+	public bool dontIfEditor = true;
+
+    public Behaviour component;
+
+    // Use this for initialization
+    void Start() {
+        if (!dontIfEditor || !Application.isEditor) {
+            component.enabled = true;
+        }
+    }
+}
