778363c38a3f36260e6f71b8c73d2ef7896f3263
Thomas Bartlett <tbartl@umich.edu>
2018-09-13 22:47:14 -0400

    Smooth attach

--- SS5R-Source/Assets/Objects/Attach/Attacher.cs
+++ SS5R-Source/Assets/Objects/Attach/Attacher.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Attacher : Interactor, IOnLetGo {
     AttachPoint attachedTo;
+    float attachTime = .3f;
+    AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
     public override bool CanInteractWith(Interactable interactable) {
         if (!base.CanInteractWith(interactable))
@@ -22,13 +24,25 @@ public class Attacher : Interactor, IOnLetGo {
 
     public void SetAttached(AttachPoint point) {
         attachedTo = point;
-        this.transform.parent = point.transform;
-        this.transform.localPosition = Vector3.zero;
-        this.transform.localRotation = Quaternion.identity;
         this.transform.parent = point.transform.parent;
+        StartCoroutine(SmoothAttach());
         Destroy(this.GetComponent<Rigidbody>());
         Destroy(this.GetComponent<Grabbable>());
     }
 
+    IEnumerator SmoothAttach() {
+        Vector3 fromPos = this.transform.localPosition;
+        Vector3 toPos = attachedTo.transform.localPosition;
+        Quaternion fromRot = this.transform.localRotation;
+        Quaternion toRot = attachedTo.transform.localRotation;
+        for (float t = 0; t < attachTime; t += Time.deltaTime) {
+            float p = t / attachTime;
+            this.transform.localPosition = Vector3.Lerp(fromPos, toPos, curve.Evaluate(p));
+            this.transform.localRotation = Quaternion.Slerp(fromRot, toRot, curve.Evaluate(p));
+            yield return null;
+        }
+        this.transform.localPosition = toPos;
+        this.transform.localRotation = toRot;
+    }
 }
 
