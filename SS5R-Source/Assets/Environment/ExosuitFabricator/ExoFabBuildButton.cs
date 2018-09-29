3d144f37d6cc11146a9e2d9952a3b844b96168ef
Thomas Bartlett <tbartl@umich.edu>
2018-09-28 23:46:07 -0400

    Started build queue

--- SS5R-Source/Assets/Environment/ExosuitFabricator/ExoFabBuildButton.cs
+++ SS5R-Source/Assets/Environment/ExosuitFabricator/ExoFabBuildButton.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ExoFabBuildButton : MonoBehaviour, IOnPressed {
-    public List<BuildInformation> buildInformations;
 
     public void OnPressed(Interactor by) {
         Debug.Log("TODO");
--- SS5R-Source/Assets/Environment/ExosuitFabricator/ExosuitFabricator.cs
+++ SS5R-Source/Assets/Environment/ExosuitFabricator/ExosuitFabricator.cs
@@ -1,11 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExosuitFabricator : MonoBehaviour {
-	public Transform spawnPosition;
+    public RectTransform buildQueueContainer;
 
-	public void TryBuild(BuildInformation buildInformation) { 
-		Instantiate(buildInformation.prefab, spawnPosition.transform.position, Quaternion.identity);
-	}
+    public Transform spawnPosition;
+    public GameObject buildButtonPrefab;
+
+    List<ExoFabBuildButton> buildQueue = new List<ExoFabBuildButton>();
+
+    public void TryBuild(BuildInformation buildInformation) {
+        Instantiate(buildInformation.prefab, spawnPosition.transform.position, Quaternion.identity);
+
+        GameObject buildGO = Instantiate(buildButtonPrefab, buildQueueContainer);
+
+        Image buildImage = buildGO.GetComponent<Image>();
+        buildImage.SetNativeSize();
+        buildImage.sprite = buildInformation.buildSprite;
+
+        RectTransform buildRT = buildGO.GetComponent<RectTransform>();
+        buildRT.localPosition = Vector3.zero;
+        buildRT.localRotation = Quaternion.identity;
+
+        ExoFabBuildButton build = buildGO.GetComponent<ExoFabBuildButton>();
+        buildQueue.Add(build);
+
+        UpdatePositions();
+
+    }
+
+    void UpdatePositions() {
+        for (int i = 0; i < buildQueue.Count; i++) {
+            RectTransform rt = buildQueue[i].GetComponent<RectTransform>();
+            rt.localPosition = new Vector3(0, -(rt.sizeDelta.y + 10) * i, 0);
+        }
+    }
 }
