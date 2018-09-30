910530850b2db8b35440b68f1ad7276281832a83
Thomas Bartlett <tbartl@umich.edu>
2018-09-29 21:41:14 -0400

    Sound on button hovered

---
+++ SS5R-Source/Assets/Environment/ExosuitFabricator/SoundOnHovered.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnHovered : MonoBehaviour, IOnHovered {
+
+    float recentlyHovered = 0f;
+
+    public AudioClip clip;
+
+    public void OnHovered(Interactor interactor) {
+        bool wasRecentlyHovered = recentlyHovered > 0;
+        recentlyHovered = .2f;
+        if (wasRecentlyHovered)
+            return;
+        AudioSource source = this.gameObject.AddComponent<AudioSource>();
+        Destroy(source, clip.length + 1);
+        source.clip = clip;
+        source.spatialBlend = 1;
+        source.Play();
+    }
+
+    void Update() {
+        recentlyHovered -= Time.deltaTime;
+    }
+}
