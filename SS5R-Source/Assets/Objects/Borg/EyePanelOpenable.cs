39d8cf299c197a16e57c62bc46bac84e80c83dba
Thomas Bartlett <tbartl@umich.edu>
2018-10-07 17:11:45 -0400

    Start borg head eye panel open

---
+++ SS5R-Source/Assets/Objects/Borg/EyePanelOpenable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyePanelOpenable : Interactable {
+    public override bool GetInteractable(Interactor interactor) {
+        return base.GetInteractable(interactor) && interactor.GetComponent<EyePanelOpener>() != null;
+    }
+
+    public override void InteractWith(Interactor interactor) {
+        Debug.Log("Open eye panel");
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ SS5R-Source/Assets/Objects/Borg/EyePanelOpener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyePanelOpener : Interactor {
+    void OnTriggerEnter(Collider other) {
+        Interactable otherInteractable = other.GetComponent<Interactable>();
+        if (!otherInteractable)
+            return;
+        if (!otherInteractable.GetInteractable(this))
+            return;
+        otherInteractable.InteractWith(this);
+    }
+}
