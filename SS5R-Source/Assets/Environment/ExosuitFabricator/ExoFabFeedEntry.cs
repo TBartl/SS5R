8624200e65b38f72e2bd7ac09d7acc13205b2cc2
Thomas Bartlett <tbartl@umich.edu>
2018-10-07 13:38:43 -0400

    Started exofab feed

---
+++ SS5R-Source/Assets/Environment/ExosuitFabricator/ExoFabFeedEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExoFabFeedEntry : Interactable {
+    public override bool GetInteractable(Interactor interactor) {
+        return base.GetInteractable(interactor); ;
+    }
+
+    public override void InteractWith(Interactor interactor) {
+        Destroy(interactor.gameObject);
+    }
+}
---
+++ SS5R-Source/Assets/Environment/ExosuitFabricator/ExoFabFeedable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Containable))]
+public class ExoFabFeedable : Interactor, IOnLetGo {
+
+    public override bool CanInteractWith(Interactable interactable) {
+        if (!base.CanInteractWith(interactable))
+            return false;
+        return interactable.GetComponent<ExoFabFeedEntry>();
+    }
+
+    public void OnLetGo(Container from) {
+        if (this.hoverObject && this.CanInteractWith(hoverObject)) {
+            this.hoverObject.GetComponent<ExoFabFeedEntry>().InteractWith(this);
+        }
+    }
+}
