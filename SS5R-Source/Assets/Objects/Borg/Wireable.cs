33660f27a5bdcf3d6a0ae922f7c0971bce73ae43
Thomas Bartlett <tbartl@umich.edu>
2018-10-07 20:26:16 -0400

    Wiring

---
+++ SS5R-Source/Assets/Objects/Borg/Wireable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wireable : Interactable {
+    [SerializeField] MeshRenderer wires;
+
+    Material replaceMaterial;
+    bool wired = false;
+
+    void Awake() {
+        replaceMaterial = wires.material;
+        wires.materials = new Material[] { };
+    }
+
+    public override bool GetInteractable(Interactor interactor) {
+        return base.GetInteractable(interactor) && wired == false && interactor.GetComponent<Wirer>() != null;
+    }
+
+    public override void InteractWith(Interactor interactor) {
+        wired = true;
+        wires.materials = new Material[] { replaceMaterial };
+        this.GetComponent<AudioSource>().Play();
+    }
+}
---
+++ SS5R-Source/Assets/Objects/Borg/Wirer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wirer : Interactor {
+    void OnTriggerEnter(Collider other) {
+        Interactable otherInteractable = other.GetComponent<Interactable>();
+        if (!otherInteractable)
+            return;
+        if (!otherInteractable.GetInteractable(this))
+            return;
+        otherInteractable.InteractWith(this);
+    }
+}
